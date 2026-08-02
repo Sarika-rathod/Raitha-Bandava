781c3d0950a6366e71ba7c1b7293fcd92cae5df0
Sarika Rathod <sarikaappurathod@gmail.com>
2026-08-02 12:15:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,6 @@ from werkzeug.security import (
 )
 from agno.agent import Agent
 from agno.models.groq import Groq
-from agno.tools.websearch import WebSearchTools
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.db.sqlite import SqliteDb
 
