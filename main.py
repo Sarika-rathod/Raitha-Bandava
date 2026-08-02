d79a59c496402cf2549a642c5af7adaee760749f
Sarika Rathod <sarikaappurathod@gmail.com>
2026-08-02 13:14:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,12 +146,13 @@ print("Step 4: Flask app created")
 
 def get_db_connection():
 
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Raitha@123",  
-        database="raitha_bandava"
-    )
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 3306)),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
 
 
 
