ce41a0c0b82fa4aad165b21ca7fadd790b58f76e
Sarika Rathod <sarikaappurathod@gmail.com>
2026-08-03 16:43:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -560,16 +560,10 @@ def predict():
         }),500
            
 if __name__ == "__main__":
-    
-
     create_tables()
     print("✅ SQLite database ready.")
 
     app.run(
-        host="127.0.0.1",
-        port=5001,
-        debug=True
-    )
         host="127.0.0.1",
         port=5001,
         debug=True
