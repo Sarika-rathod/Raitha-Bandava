f0b405d0de297f8417a4b32b7fc2cc55d1b93dd7
Sarika Rathod <sarikaappurathod@gmail.com>
2026-09-07 18:03:08 +0530

    Change app run configuration for production

--- main.py
+++ main.py
@@ -577,8 +577,10 @@ def predict():
         }), 500
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+
     app.run(
-        host="127.0.0.1",
-        port=5001,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
